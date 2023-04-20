401f921eadd332efe9451b77468957d0f938ed15
kemiedon <kemiedon@gmail.com>
2023-04-20 17:20:51 +0800

    V1.4 2023-04-20

--- 02_Interactive/js/photo_drag.js
+++ 02_Interactive/js/photo_drag.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
             // 讀取圖片檔案
             var reader = new FileReader();
             reader.onload = function (e) {
-                // 創建圖片元素，設定拖放效果的初始樣式
+                // 建立圖片元素，設定拖放效果的初始樣式
                 var photo = $("<div>").addClass("photo");
 
                 var img = $("<img>").attr("src", e.target.result);
@@ -76,4 +76,34 @@ $(document).ready(function () {
             reader.readAsDataURL(file);
         }
     });
+    $('#download').click(function(){
+        // 獲取所有照片
+        var photos = $(".photo");
+        // 建立一個Canvas元素
+        var canvas = document.createElement("canvas");
+        var ctx = canvas.getContext("2d");
+        // 設定Canvas元素的寬高為照片區域的寬高
+        canvas.width = photoArea.width();
+        canvas.height = photoArea.height();
+        // 將所有照片繪製到Canvas元素上
+        photos.each(function (index, photo) {
+            // 獲取照片在photoArea中的相對位置和大小
+            var position = $(photo).position();
+            var width = $(photo).width();
+            var height = $(photo).height();
+            // 繪製照片到Canvas元素上
+            ctx.drawImage(photo.firstChild, position.left, position.top, width, height);
+        });
+        // 將Canvas元素轉換為Data URL
+        var dataURL = canvas.toDataURL();
+        // 建立一個下載連結，將Data URL 設為下載連結的 href 屬性
+        var downloadLink = document.createElement("a");
+        downloadLink.href = dataURL;
+        // 設定下載檔案的檔名
+        downloadLink.download = Math.floor(Math.random() * 10000) + ".png";
+        // 模擬點擊下載連結，觸發下載功能
+        downloadLink.click();
+    })
+    
+    
 });
